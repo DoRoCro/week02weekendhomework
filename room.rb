e0cc81d338a55fb2bff05d3740cf9efebc479fd7
Douglas Crooke <douglas.crooke+codeclan@googlemail.com>
2017-02-11 23:00:40 +0000

    add songlist method to room

--- room.rb
+++ room.rb
@@ -4,4 +4,8 @@ class Room
     @name = name
     @songlist = []
   end
+
+  def add_songs(list_of_songs)
+    @songlist.concat(list_of_songs) 
+  end
 end
--- specs/room_spec.rb
+++ specs/room_spec.rb
@@ -2,6 +2,7 @@ require('minitest/autorun')
 require('minitest/rg')
 require_relative('../room')
 require_relative('../song')
+require('pry')
 
 class TestRoom < Minitest::Test
 
@@ -10,9 +11,9 @@ class TestRoom < Minitest::Test
     
     @song1 = Song.new("My Way", "Frank Sinatra", "Start spreading...")
     @song2 = Song.new("I Will Survive", "Goria Gaynor", "At first I was ...")
-    @song3 =  Song.new("Is this the way to Amarillo", "Tony Christie", "La La La...")
+    @song3 = Song.new("Is this the way to Amarillo", "Tony Christie", "La La La...")
     
-    @songlist = [@song1, @song2, @song3]
+    @a_songlist = [@song1, @song2, @song3]
 
     @room1 = Room.new(room1name)
      
@@ -27,7 +28,8 @@ class TestRoom < Minitest::Test
   end
 
   def test_can_add_song_to_room_songlist
-
+    @room1.add_songs(@a_songlist)
+    assert_equal([@song1, @song2, @song3], @room1.songlist)
   end
 
 end
