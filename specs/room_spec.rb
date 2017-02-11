0ecfd2b8b95aa34dfb1e54e0c0aa4bb05b253cb1
Douglas Crooke <douglas.crooke+codeclan@googlemail.com>
2017-02-11 22:41:35 +0000

    refactor room songs setup

--- specs/room_spec.rb
+++ specs/room_spec.rb
@@ -8,11 +8,11 @@ class TestRoom < Minitest::Test
   def setup
     room1name = "alpha"
     
-    song1 = Song.new("My Way", "Frank Sinatra", "Start spreading...")
-    song2 = Song.new("I Will Survive", "Goria Gaynor", "At first I was ...")
-     song3 =  Song.new("Is this the way to Amarillo", "Tony Christie", "La La La...")
+    @song1 = Song.new("My Way", "Frank Sinatra", "Start spreading...")
+    @song2 = Song.new("I Will Survive", "Goria Gaynor", "At first I was ...")
+    @song3 =  Song.new("Is this the way to Amarillo", "Tony Christie", "La La La...")
     
-    @songlist = [song1, song2, song3]
+    @songlist = [@song1, @song2, @song3]
 
     @room1 = Room.new(room1name)
      
@@ -24,6 +24,9 @@ class TestRoom < Minitest::Test
 
   def test_new_room_has_empty_songlist
     assert_equal([], @room1.songlist)
+  end
+
+  def test_can_add_song_to_room_songlist
 
   end
 
