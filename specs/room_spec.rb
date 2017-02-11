46fe34910d3af7a51662f95db227c5125db0f835
Douglas Crooke <douglas.crooke+codeclan@googlemail.com>
2017-02-11 22:27:59 +0000

    refactor room test setup

--- specs/room_spec.rb
+++ specs/room_spec.rb
@@ -4,10 +4,18 @@ require_relative('../room')
 
 class TestRoom < Minitest::Test
 
+  def setup
+    room1name = "alpha"
+    @room1 = Room.new(room1name)
+    
+  end
+
   def test_room_has_name
-    room1name = "aplha"
-    room1 = Room.new(room1name)
-    assert_equal(room1name, room1.name)
+    assert_equal('alpha', @room1.name)
+  end
+
+  def test_room_has_songlist
+
   end
 
 end
