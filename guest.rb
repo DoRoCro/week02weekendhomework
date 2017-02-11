feb5f321560c709ca8a6f08aade1b3b9ea31b011
Douglas Crooke <douglas.crooke+codeclan@googlemail.com>
2017-02-11 23:21:04 +0000

    add change_favourite_song method to guest

--- guest.rb
+++ guest.rb
@@ -1,9 +1,16 @@
+require_relative('song')
 class Guest
 
-  attr_reader :name
+  attr_reader :name, :favourite_song
 
   def initialize(name)
     @name = name
+    @favourite_song = nil
+  end
+
+  def change_favourite_song(new_fav_song)
+    @favourite_song = new_fav_song
+    return
   end
 
 end
--- specs/guest_spec.rb
+++ specs/guest_spec.rb
@@ -5,11 +5,15 @@ require_relative('../guest')
 class TestGuest < Minitest::Test
   def setup
     @guest1 = Guest.new("Dave")
+    @song1 = Song.new("My Way", "Frank Sinatra", "Start spreading...")
   end
-  
+
   def test_guest_has_name
     assert_equal("Dave", @guest1.name)
   end
 
-
+  def test_guest_can_change_favourite_song
+    @guest1.change_favourite_song(@song1)
+    assert_equal(@song1, @guest1.favourite_song)
+  end
 end
