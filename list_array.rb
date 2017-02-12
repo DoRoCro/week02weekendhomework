e203353e3e0e19bfa97154c668636e318717f13a
Douglas Crooke <douglas.crooke+codeclan@googlemail.com>
2017-02-12 12:24:44 +0000

    add add_element, add_list, empty_list methods

--- list_array.rb
+++ list_array.rb
@@ -1,7 +1,25 @@
 class ListArray
+  
   attr_reader :contents
-def initialize()
-  return @contents = []
-end
+  
+  def initialize()
+    @contents = []
+  end
+
+  def add_element_to_list(an_object)
+    @contents << an_object
+  end
+
+  def add_list_to_list(a_list)
+    @contents.concat(a_list)
+  end
+
+  def empty_list()
+    return_list = @contents
+    @contents = []
+    return return_list
+  end
+
+  
 
 end
--- specs/list_array_spec.rb
+++ specs/list_array_spec.rb
@@ -1,11 +1,36 @@
 require('minitest/autorun')
 require('minitest/rg')
 require_relative('../list_array')
+# require('pry')
 
 class TestListArray < Minitest::Test
 
+  def setup
+    @object = { dummy: "a string"}
+  end
+
   def test_create_list_array_is_empty_array
     assert_equal([], ListArray.new().contents )
+  end
+
+  def test_add_element_to_array
+    list1 = ListArray.new
+    list1.add_element_to_list(@object)
+    assert_equal([ @object ], list1.contents )
+  end
+
+  def test_add_list_of_elements_to_array
+    list1 = ListArray.new
+    list1.add_list_to_list([@object, @object])
+    assert_equal(2, list1.contents.count )
+  end
+
+  def test_can_empty_list_and_return_contents
+    list1 = ListArray.new
+    list1.add_list_to_list([@object, @object])
+    extracted = list1.empty_list()
+    assert_equal(0, list1.contents.count )
+    assert_equal([@object, @object], extracted )
   end
 
 end
